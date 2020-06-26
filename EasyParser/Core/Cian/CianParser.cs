1010042d573c1d31c0fbc73f938295a4bd80ecb9
Evgeniy Grigorev <timeinvest27@gmail.com>
2020-06-26 14:24:38 +0300

    Добавил классы для парса и настроики парса для сайта Cian

---
+++ EasyParser/Core/Cian/CianParser.cs
@@ -0,0 +1,27 @@
+﻿using AngleSharp.Html.Dom;
+using EasyParser.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyParser.Core.Cian
+{
+    class CianParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            List<string> list = new List<string>();
+            var items = document.QuerySelectorAll(".c6e8ba5398--info-container--A11gU").Where(x => x.ClassName != null);
+
+            foreach (var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+        }
+
+            
+    }
+}
