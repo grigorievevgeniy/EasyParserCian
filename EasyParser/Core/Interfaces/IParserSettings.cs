3461efaadf5f96b81d4612b126d4b3922d3bbddf
Evgeniy Grigorev <timeinvest27@gmail.com>
2020-06-26 12:36:06 +0300

    Добавил интерфейсы для парсера и его настроек

---
+++ EasyParser/Core/Interfaces/IParserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyParser.Core.Interfaces
+{
+    interface IParserSettings
+    {
+        string BaseUrl { get; set; }
+        string Prefix { get; set; }
+
+        int StartPage { get; set; }
+        int EndPage { get; set; }
+    }
+}
